6e9bece6b26db70135a7abe4c7a33cc3c6f640a5
c-xpanxion-mmariapp <mathesh.mariappan@turner.com>
2020-08-28 16:49:15 +0530

    addde

---
+++ Helloworld/src/Hello/Helloworld.java
@@ -0,0 +1,16 @@
+package Hello;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=10;
+		for (int j = 0; j<i; j++) {
+			System.out.println("The number is "+j);
+			
+		}
+
+	}
+
+}
